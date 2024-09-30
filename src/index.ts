da374705f197aea52ff6d22e6db576986ecf3ffe
Nikita TEREKHOV <nikita.a.terekhov@gmail.com>
2024-09-30 13:24:35 +0800

    chore: few minor changes

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,25 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-import { sdkApi } from "./app";
-import { RedisCacheStorage } from "./cache/redis.storage";
 import { InMemoryCacheStorage } from "@cedelabs-private/sdk";
+import { sdkApi } from "./app";
 import { hmacAuthStrategyMiddleware } from "./authentication/hmac.strategy";
+import { RedisCacheStorage } from "./cache/redis.storage";
+
+const getCache = () => {
+	if (process.env.NODE_ENV === "development" || !process.env.REDIS_URL) {
+		console.log("[server]: 🚧 You are using an in-memory cache. Please use Redis cache in production.");
+		return new InMemoryCacheStorage();
+	}
+	return RedisCacheStorage.create({
+		url: process.env.REDIS_URL,
+	});
+}
 
 sdkApi({
 	mode: "REAL",
 	clientId: process.env.CLIENT_ID || "",
-	cache:
-		process.env.NODE_ENV === "development"
-			? new InMemoryCacheStorage()
-			: RedisCacheStorage.create({
-					url: process.env.REDIS_URL || "",
-			  }),
+	cache: getCache(),
 	authentication: hmacAuthStrategyMiddleware({
 		secretKey:  process.env.SECRET_API_KEY || "",
 	}),
